d7ffe7e79a293187dcb2a86c99af839cca57032b
Sai Kumar M K <saikumarmk2902@gmail.com>
2025-02-10 08:02:32 +0530

    prime

--- Lab1.py
+++ Lab1.py
@@ -257,3 +257,19 @@
 # for row in result:
 #     print(row)
 
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if is_prime(num):
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
